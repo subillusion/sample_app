c058649a958b02a85071aa842bdbd8080b8345ba
Timothy Danner <dannerman@hotmail.com>
2019-06-17 01:31:26 +0000

    Finish user edit, update, index, and destroy options

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "Timothy Danner",
+             email: "dannerman@hotmail.com",
+             password:              "Test1234",
+             password_confirmation: "Test1234",
+             admin: true)
+
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "Test1234"
+  User.create!(name:  name,
+               email: email,
+               password:              password,
+               password_confirmation: password)
+end
